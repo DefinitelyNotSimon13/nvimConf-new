e18830be1a960326e4a6759b131c1e84efcc1763
Simon <simon21.blum@gmail.com>
2024-04-10 15:21:22 +0200

    Update LspConfig

--- lua/plugins/language/lspconfig.lua
+++ lua/plugins/language/lspconfig.lua
@@ -109,7 +109,18 @@ return { -- LSP Configuration & Plugins
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
+      html = {},
       clangd = {},
+      texlab = {},
+      rust_analyzer = {
+        settings = {
+          ['rust-analyzer'] = {
+            checkOnSave = {
+              command = 'clippy',
+            },
+          },
+        },
+      },
       -- gopls = {},
       -- pyright = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
